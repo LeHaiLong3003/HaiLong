1ad64d6af64aad6a9eb28ff3bcd05f645a49e3f2
SonVu08 <thiensonvu08@gmail.com>
2023-01-21 10:29:01 +0700

    Create Project

---
+++ src/app/store.js
@@ -0,0 +1,27 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+
+import { history } from '../utils/history';
+import rootSaga from './rootSaga';
+
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const configStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }).concat(sagaMiddleware, routerMiddleware(history)),
+  });
+  sagaMiddleware.run(rootSaga);
+  return store;
+};
+const store = configStore();
+
+export default store;
