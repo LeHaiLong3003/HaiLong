78a0e71db264ef3b51b945a12d6c1b64a7001d07
SonVu08 <sonvu.greenifyvn@gmail.com>
2023-02-13 02:15:34 +0700

    UI Category List

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,42 +1,34 @@
 export const categories = [
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://tuhocdohoa.vn/wp-content/uploads/2018/04/v%E1%BA%BD-thi%E1%BA%BFt-k%E1%BA%BF.jpg',
+    name: 'Thiết kế',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgaTVlRu-riwqXy3GMIP9izQl1u0WQUh7YNA&usqp=CAU',
+    name: 'Phát triển Web',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://khoinganhtruyenthong.com/wp-content/uploads/2022/08/what-is-marketing.png',
+    name: 'Marketing',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://reviewedu.net/wp-content/uploads/2021/11/94469-1.png',
+    name: 'Âm nhạc',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmsPRR9e7r7UHG5j-kTTxREdjwqtT-vsSZbg&usqp=CAU',
+    name: 'CNTT và Phần mềm',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHQ8_Yt6wUDMFkreOdkn_DQhp5sFOGpBUgog&usqp=CAU',
+    name: 'Kinh doanh',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://xuconcept.com/wp-content/uploads/2015/02/setup-den-chup-trong-studio.jpg',
+    name: 'Nhiếp ảnh',
   },
   {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
-  },
-  {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
-  },
-  {
-    imageUrl: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
-    name: 'React Js',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc2A8rzdgx0T35-8-nSv8l36anOKzf9u0kJw&usqp=CAU',
+    name: 'Phát triển cá nhân',
   },
 ];
